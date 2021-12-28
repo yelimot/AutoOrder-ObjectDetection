0b36607f6b6a1a98e016a02eafe439f47d60b514
root <root@instance-1.europe-west1-b.c.pure-harbor-336319.internal>
2021-12-28 13:45:52 +0000

    print

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/analyse', methods=['POST'])
 def index():
-    print(str(request))
+    print(str(request.form), str(request.files))
     config = request.form.get("config")
     f = request.files['image']
     result = td.analyse(f,config)
